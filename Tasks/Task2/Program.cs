f75553b2916cca4c2f49cb03e92f12ba80f1cd96
AnyutaAntonovich <antonovich_am@bk.ru>
2023-06-02 23:15:25 +0600

    Add task 2

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Promt("Input the number:");
+System.Console.WriteLine($"Sum all digit in number {number} = {SumAllDigit(number)}");
