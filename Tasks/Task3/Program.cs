0bce84e5833ff747367d61d3a53b5d551f335859
AnyutaAntonovich <antonovich_am@bk.ru>
2023-06-03 00:34:36 +0600

    Add task 3

---
+++ Tasks/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.WriteLine("]");
+}
+
+int length = Promt("Length of array:");
+int min = Promt("initial value for random number range:");
+int max = Promt("end value for a range of random number:");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
